4f6d4c8f649cc655e9e65aa191ef46ce93d5c0f8
Timm Bahmann <timm@bahmann.one>
2020-02-18 13:10:05 +0100

    remove emforcement of api token

--- src/index.js
+++ src/index.js
@@ -8,14 +8,7 @@ import {
 import downloadPage from './downloadPage'
 import { queryCollection } from './notionMethods'
 
-function Notion() {
-  if (!process.env.TOKEN_V2) {
-    throw new Error(`Your TOKEN_V2 is undefined. 
-    You need it to use this API. You can find yours on your Notion page by looking at the cookies while logged in. 
-    For further help, please open an issue at https://github.com/Gamaranto/notionjs
-    `)
-  }
-}
+function Notion() {}
 
 Notion.prototype.getBlock = getBlock
 Notion.prototype.getCollection = getCollection
